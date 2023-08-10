3174e2757b3db5634dec6f01089e65081e627657
SStakafumi <gtaka1213@gmail.com>
2023-08-10 21:20:16 +0900

    my python script for p2ch10

--- p2ch10/my_dsets.py
+++ p2ch10/my_dsets.py
@@ -30,6 +30,7 @@ CandidateInfoTuple = namedtuple(
     'isNodule_bool, diameter_mm, series_uid, center_xyz',
 )
 
+
 @functools.lru_cache(1)
 def getCandidateInfoList(requireOnDisk_bool=True):
     # We construct a set with all series_uids that are present on disk.
@@ -64,7 +65,8 @@ def getCandidateInfoList(requireOnDisk_bool=True):
             for annotation_tup in diameter_dict.get(series_uid, []):
                 annotationCenter_xyz, annotationDiameter_mm = annotation_tup
                 for i in range(3):
-                    delta_mm = abs(candidateCenter_xyz[i] - annotationCenter_xyz[i])
+                    delta_mm = abs(
+                        candidateCenter_xyz[i] - annotationCenter_xyz[i])
                     if delta_mm > annotationDiameter_mm / 4:
                         break
                 else:
@@ -81,20 +83,26 @@ def getCandidateInfoList(requireOnDisk_bool=True):
     candidateInfo_list.sort(reverse=True)
     return candidateInfo_list
 
+
 class Ct:
+    """
+    識別子を引数に取り, CT画像を読み込み, xyz座標をirc座標に変換し、結節周りの画像を切り出すクラス
+    """
+
     def __init__(self, series_uid):
         mhd_path = glob.glob(
             'data-unversioned/part2/luna/subset*/{}.mhd'.format(series_uid)
         )[0]
 
         ct_mhd = sitk.ReadImage(mhd_path)
-        ct_a = np.array(sitk.GetArrayFromImage(ct_mhd), dtype=np.float32)
+        ct_a = np.array(sitk.GetArrayFromImage(ct_mhd),
+                        dtype=np.float32)  # [Z, Y, X]
 
         # CTs are natively expressed in https://en.wikipedia.org/wiki/Hounsfield_scale
         # HU are scaled oddly, with 0 g/cc (air, approximately) being -1000 and 1 g/cc (water) being 0.
         # The lower bound gets rid of negative density stuff used to indicate out-of-FOV
         # The upper bound nukes any weird hotspots and clamps bone down
-        ct_a.clip(-1000, 1000, ct_a)
+        ct_a.clip(-1000, 1000, ct_a)  # <- modifies ct_a in place
 
         self.series_uid = series_uid
         self.hu_a = ct_a
@@ -104,7 +112,8 @@ class Ct:
         self.direction_a = np.array(ct_mhd.GetDirection()).reshape(3, 3)
 
     def getRawCandidate(self, center_xyz, width_irc):
-        center_irc = xyz2irc(
+        """結節周りの画像を切り出す"""
+        center_irc = xyz2irc(  # xyz座標をirc座標に変換
             center_xyz,
             self.origin_xyz,
             self.vxSize_xyz,
@@ -113,72 +122,82 @@ class Ct:
 
         slice_list = []
         for axis, center_val in enumerate(center_irc):
-            start_ndx = int(round(center_val - width_irc[axis]/2))
-            end_ndx = int(start_ndx + width_irc[axis])
+            start_ndx = int(round(center_val - width_irc[axis]/2))  # 切り出しの開始位置
+            end_ndx = int(start_ndx + width_irc[axis])  # 切り出しの終了位置
 
-            assert center_val >= 0 and center_val < self.hu_a.shape[axis], repr([self.series_uid, center_xyz, self.origin_xyz, self.vxSize_xyz, center_irc, axis])
+            assert center_val >= 0 and center_val < self.hu_a.shape[axis], repr(
+                [self.series_uid, center_xyz, self.origin_xyz, self.vxSize_xyz, center_irc, axis])
 
+            # 切り出しの開始位置が画像の範囲外の場合
             if start_ndx < 0:
-                # log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
-                #     self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
                 start_ndx = 0
                 end_ndx = int(width_irc[axis])
 
+            # 切り出しの終了位置が画像の範囲外の場合
             if end_ndx > self.hu_a.shape[axis]:
-                # log.warning("Crop outside of CT array: {} {}, center:{} shape:{} width:{}".format(
-                #     self.series_uid, center_xyz, center_irc, self.hu_a.shape, width_irc))
                 end_ndx = self.hu_a.shape[axis]
                 start_ndx = int(self.hu_a.shape[axis] - width_irc[axis])
 
+            # 切り出し
             slice_list.append(slice(start_ndx, end_ndx))
 
+        # 切り出した画像を返す
         ct_chunk = self.hu_a[tuple(slice_list)]
 
+        # 切り出した画像と中心座標を返す
         return ct_chunk, center_irc
 
 
 def getCt(series_uid):
     return Ct(series_uid)
 
-@raw_cache.memoize(typed=True)
+
+@raw_cache.memoize(typed=True)  # メモ化することで高速化
 def getCtRawCandidate(series_uid, center_xyz, width_irc):
+    """
+    結節周りの画像を切り出す
+    """
     ct = getCt(series_uid)
     ct_chunk, center_irc = ct.getRawCandidate(center_xyz, width_irc)
     return ct_chunk, center_irc
 
+
 class LunaDataset(Dataset):
-    def __init__(self,
-                 val_stride=0,
-                 isValSet_bool=None,
-                 series_uid=None,
-            ):
-        self.candidateInfo_list = copy.copy(getCandidateInfoList())
+    """
+    各CTインスタンスに対してPytorchのデータセットに変換するクラス
+    """
 
-        if series_uid:
-            self.candidateInfo_list = [
-                x for x in self.candidateInfo_list if x.series_uid == series_uid
-            ]
+    def __init__(self, val_stride=0, isValSet_bool=None, series_uid=None,):
+        self.conadidateInfo_list = copy.copy(
+            getCandidateInfoList())  # candidateInfo_listをコピー
 
-        if isValSet_bool:
-            assert val_stride > 0, val_stride
-            self.candidateInfo_list = self.candidateInfo_list[::val_stride]
-            assert self.candidateInfo_list
-        elif val_stride > 0:
-            del self.candidateInfo_list[::val_stride]
-            assert self.candidateInfo_list
-        random.shuffle(self.candidateInfo_list)
+        if series_uid:  # series_uidが指定されている場合
+            self.conadidateInfo_list = [  # series_uidに一致するものだけに絞る
+                x for x in self.conadidateInfo_list if x.series_uid == series_uid]  # xは名前付きタプル
 
-        log.info("{!r}: {} {} samples".format(
+        if isValSet_bool:  # isValSet_boolが指定されている場合(検証データセットの場合)
+            assert val_stride > 0, val_stride  # val_strideが0より大きいことを確認
+            # val_strideごとにサンプリング
+            self.conadidateInfo_list = self.conadidateInfo_list[::val_stride]
+            assert self.conadidateInfo_list  # self.conadidateInfo_listが空でないことを確認
+        elif val_stride > 0:  # isValSet_boolが指定されていない場合(学習データセットの場合)
+            del self.conadidateInfo_list[::val_stride]
+            assert self.conadidateInfo_list
+
+        random.shuffle(self.conadidateInfo_list)  # ランダムに並び替え
+
+        log.info("{!r}: {} {} samples".format(  # !rはrepr()を呼び出す
             self,
-            len(self.candidateInfo_list),
+            len(self.conadidateInfo_list),
             "validation" if isValSet_bool else "training",
         ))
 
     def __len__(self):
-        return len(self.candidateInfo_list)
+        return len(self.conadidateInfo_list)
 
     def __getitem__(self, ndx):
-        candidateInfo_tup = self.candidateInfo_list[ndx]
+        # candidateInfo_listからndx番目の要素を取り出す
+        candidateInfo_tup = self.conadidateInfo_list[ndx]
         width_irc = (32, 48, 48)
 
         candidate_a, center_irc = getCtRawCandidate(
@@ -189,18 +208,12 @@ class LunaDataset(Dataset):
 
         candidate_t = torch.from_numpy(candidate_a)
         candidate_t = candidate_t.to(torch.float32)
-        candidate_t = candidate_t.unsqueeze(0)
+        candidate_t = candidate_t.unsqueeze(0)  # チャンネル次元を追加
 
-        pos_t = torch.tensor([
-                not candidateInfo_tup.isNodule_bool,
-                candidateInfo_tup.isNodule_bool
-            ],
-            dtype=torch.long,
-        )
+        pos_t = torch.tensor([  # 結節の有無を表すラベルを作成
+            # 結節ならtorch.tensor[False, True] -> [0, 1], 結節でないならtorch.tensor[True, False] -> [1, 0]
+            not candidateInfo_tup.isNodule_bool,
+            candidateInfo_tup.isNodule_bool
+        ], dtype=torch.long)
 
-        return (
-            candidate_t,
-            pos_t,
-            candidateInfo_tup.series_uid,
-            torch.tensor(center_irc),
-        )
+        return (candidate_t, pos_t, candidateInfo_tup.series_uid, torch.tensor(center_irc))
