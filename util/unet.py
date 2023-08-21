4c19e586c1b1a72bc1739d76dc1b0a17ec64ca07
SStakafumi <gtaka1213@gmail.com>
2023-08-21 21:43:28 +0900

    modified UNET model script

--- util/unet.py
+++ util/unet.py
@@ -2,19 +2,19 @@
 # https://raw.githubusercontent.com/jvanvugt/pytorch-unet/master/unet.py
 
 # MIT License
-#
+
 # Copyright (c) 2018 Joris
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
+
+# Permission is hereby granted, free of charge, to any person obtaining a co
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-#
+
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-#
+
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -137,7 +137,7 @@ class UNetUpBlock(nn.Module):
     def forward(self, x, bridge):
         up = self.up(x)
         crop1 = self.center_crop(bridge, up.shape[2:])
-        out = torch.cat([up, crop1], 1)
+        out = torch.cat([up, crop1], 1) # スキップ接続
         out = self.conv_block(out)
 
         return out
